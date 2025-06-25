ce26ffa3b0c3ca4f422a4a96f431b511d0f9e43a
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-06-25 13:35:49 +0300

    trying to make car work

--- scripts/base_car.gd
+++ scripts/base_car.gd
@@ -2,54 +2,50 @@ extends Node2D
 
 @onready var fix_l: Node2D = $base_car/FixL
 @onready var fix_r: Node2D = $base_car/FixR
-@onready var wheel_l: CharacterBody2D = $WheelL
-@onready var wheel_r: CharacterBody2D = $WheelR
+@onready var wheel_l: RigidBody2D = $WheelL
+@onready var wheel_r: RigidBody2D = $WheelR
 @onready var base_car: RigidBody2D = $base_car
 @onready var car_stabilizer_l: StaticBody2D = $WheelL/CarStabilizerL
 @onready var car_stabilizer_r: StaticBody2D = $WheelR/CarStabilizerR
 @export var wheels_distance_fix : float = 12.2
-var saved_velocity : Array[Vector2]
-
-
-func _ready() -> void:
-	saved_velocity = [Vector2.ZERO, Vector2.ZERO]
+var is_amor_l_touching : bool = false
+var is_amor_r_touching : bool = false
+var wheel_distance : Array[float]
 
 
 func _physics_process(delta: float) -> void:
-#handling gravity for every movable part of the car
-	var character_bodies_array = get_tree().get_nodes_in_group("CharacterBodies")
-	for i in character_bodies_array.size():
-		var body = character_bodies_array[i]
-		if body.get_class() == "CharacterBody2D":
-			if not body.is_on_floor():
-				body.velocity.y += 980 * delta
-				if body.name == "WheelL":
-					saved_velocity[0].y += 10
-				elif body.name == "WheelR":
-					saved_velocity[1].y += 10
-			else: 
-				if wheel_l.is_on_floor():
-					saved_velocity[0].y = 0
-				elif wheel_r.is_on_floor():
-					saved_velocity[1].y = 0
-			body.move_and_slide()
-
 	_handling_wheels_fix(delta)
-
-	car_stabilizer_l.rotation = base_car.rotation
-	car_stabilizer_r.rotation = base_car.rotation
+	base_car.apply_force(Vector2.RIGHT * 520)
+	wheel_l.apply_force(Vector2.RIGHT * 520)
+	wheel_r.apply_force(Vector2.RIGHT * 520)
 
 
 func _handling_wheels_fix(delta: float):
-	print(get_wheel_distance())
-# calc for left wheel
+	wheel_distance = get_wheel_distance()
 	if (get_wheel_distance()[0] > wheels_distance_fix):
-		wheel_l.velocity += (fix_l.global_position - wheel_l.global_position) * 3
-	else: wheel_l.velocity = saved_velocity[0]
+		wheel_l.apply_force(fix_l.global_position - wheel_l.global_position)
 	if (get_wheel_distance()[1] > wheels_distance_fix):
-		wheel_r.velocity += (fix_r.global_position - wheel_r.global_position) * 3
-	else: wheel_r.velocity = saved_velocity[1]
+		wheel_r.apply_force(fix_r.global_position - wheel_r.global_position)
+
 
 func get_wheel_distance() -> Array[float]:
 	return [wheel_l.global_position.distance_to(fix_l.global_position), 
 	wheel_r.global_position.distance_to(fix_r.global_position)]
+
+
+func _on_amor_l_environment_touched(body: Node2D) -> void:
+	print("MAMORL")
+	is_amor_l_touching = true
+
+
+func _on_amor_r_environment_touched(body: Node2D) -> void:
+	is_amor_r_touching = true
+	print("MAMORR")
+
+
+func _on_amor_l_body_exited(body: Node2D) -> void:
+	is_amor_l_touching = false 
+
+
+func _on_amor_r_body_exited(body: Node2D) -> void:
+	is_amor_r_touching = false
