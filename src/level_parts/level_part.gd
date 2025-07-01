0a8ee1a3299160c641e2d2b94612d569f243f06c
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-07-01 19:34:23 +0300

    levels created

--- src/level_parts/level_part.gd
+++ src/level_parts/level_part.gd
@@ -1,3 +1,25 @@
+@tool
 extends StaticBody2D
 class_name LevelPart
 @export var difficulty_level : int 
+@export var end_position : Vector2
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		sync()
+	position = get_parent().end_position
+
+
+func sync():
+
+ ## Syncing Line2D and Polygon2D points with CollisionPolygon2D
+
+	var line_new_array : PackedVector2Array
+	line_new_array = $CollisionPolygon2D.polygon
+	line_new_array.resize(line_new_array.size() - 2)
+	$Polygon2D.polygon = $CollisionPolygon2D.polygon
+	$Line2D.points = line_new_array
+
+## Syncing Line2D last point with end point
+
+	end_position = $Line2D.points[-1]
