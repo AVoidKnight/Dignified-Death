35728dca7a27c93826bc1e49e25f1f8e59146a8e
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-07-10 23:01:46 +0300

    added upgrade resource

---
+++ src/resources/base_upgrade.gd
@@ -0,0 +1,5 @@
+extends Resource
+@export var upgrade_dictionary : Dictionary = {"car_1_fuel_current_upgrade" : 0,
+"car_1_engine_current_upgrade" : 0,
+"car_1_n2o_current_upgrade" : 0,
+"car_1_penetration_current_upgrade" : 0}
