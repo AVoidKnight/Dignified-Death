8a8d66f3cd038fb02bd7a147ea41647d1163ffd2
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-07-12 22:30:12 +0300

    car is not movable in upgrade level

--- src/level_parts/upgrade_level.gd
+++ src/level_parts/upgrade_level.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 func _ready() -> void:
-	#$basic_ui/basic_ui/FuelMargin/FuelProgressbar.hide()
-	pass
+	$basic_ui/basic_ui/FuelMargin/FuelProgressbar.hide()
+	$base_car.is_movable = false
 
 
 func _on_button_pressed() -> void:
