2dbee6145152611e8a39ff28991805363d5387a5
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-07-24 22:49:55 +0300

    added save resource

---
+++ src/resources/save_resource.gd
@@ -0,0 +1,9 @@
+class_name SaveResource
+extends Node
+@export var money : int = 0
+@export var upgrade_dictionary_1 : Dictionary = {
+"car_1_fuel_current_upgrade" : 0,
+"car_1_engine_current_upgrade" : 0,
+"car_1_n2o_current_upgrade" : 0,
+"car_1_penetration_current_upgrade" : 0
+}
