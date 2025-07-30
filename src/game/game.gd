32c7774f37f63d1c1c6bf6d80dccf1d5501dc6d6
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-07-31 01:01:36 +0300

    main game scene became main game scene

--- src/game/game.gd
+++ src/game/game.gd
@@ -1,17 +1,2 @@
 extends Node2D
-
-
-func _on_tr_pressed() -> void:
-	WindowManagement.move_to_tr_corner()
-
-
-func _on_tl_pressed() -> void:
-	WindowManagement.move_to_tl_corner()
-
-
-func _on_bl_pressed() -> void:
-	WindowManagement.move_to_bl_corner()
-
-
-func _on_br_pressed() -> void:
-	WindowManagement.move_to_br_corner()
+var end_position = Vector2.ZERO
