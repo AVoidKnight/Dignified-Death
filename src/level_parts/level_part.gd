6c763363c752e8201018905699ab7c8a9eed7d09
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-07-31 01:01:59 +0300

    base level part update

--- src/level_parts/level_part.gd
+++ src/level_parts/level_part.gd
@@ -1,15 +1,17 @@
-@tool
 extends StaticBody2D
 class_name LevelPart
-
+@export var level_number : int
 @export var end_position : Vector2
+var player
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		sync()
 	if ("end_position" in get_parent()) == true:
 		position = get_parent().end_position
-
+	if get_parent().name == "game":
+		LevelManagement.rlg(self)
+	player = get_tree().get_first_node_in_group("player")
 
 func sync():
 
@@ -24,3 +26,16 @@ func sync():
 ## Syncing Line2D last point with end point
 
 	end_position = $Line2D.points[-1]
+
+
+func _on_degen_hitbox_body_entered(body: Node2D) -> void:
+	queue_free()
+
+
+func _on_regen_hitbox_body_entered(body: Node2D) -> void:
+	LevelManagement.rlg(get_parent())
+
+
+func _physics_process(delta: float) -> void:
+	if player.global_position.x + 100 > self.global_position.x + end_position.x and level_number != 0:
+		queue_free()
