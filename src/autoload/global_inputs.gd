15f97450e887a527cf1000610dec7bd7a92b2d28
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-08-01 23:02:48 +0300

    debug clickthrough implementation

--- src/autoload/global_inputs.gd
+++ src/autoload/global_inputs.gd
@@ -1,8 +1,12 @@
 extends Node
 
-func _input(_event: InputEvent) -> void:
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("debugging_state"):
 		WindowManagement.debug_window_change()
 	elif Input.is_action_just_pressed("testing_state"):
 		WindowManagement.test_window_change()
 		PlayerManagement.save()
+	if Input.is_key_pressed(KEY_ALT):
+		TransparentWindow.SetClickThrough(true)
+	else:
+		TransparentWindow.SetClickThrough(false)
