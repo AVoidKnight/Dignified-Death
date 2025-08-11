0906e150a4938da232403a89d4a58dc0cd103c11
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-08-11 22:46:17 +0300

    upgrade panel added 2

--- src/ui/upgrade_ui.gd
+++ src/ui/upgrade_ui.gd
@@ -1,4 +1,5 @@
 extends Control
+@onready var upgrade_panel: PanelContainer = $MarginContainer/upgrade_panel
 
 func _ready() -> void:
 	WindowManagement.corner_changed.connect(_on_corner_changed)
@@ -11,3 +12,28 @@ func _on_corner_changed():
 			get_tree().get_first_node_in_group("panel_with_play_button").size_flags_horizontal = 8
 		WindowManagement.BOTTOM_RIGHT:
 			get_tree().get_first_node_in_group("panel_with_play_button").size_flags_horizontal = 0
+
+
+func _on_fuel_button_pressed() -> void:
+	upgrade_panel.states = upgrade_panel.states_enum.FUEL
+
+
+func _on_engine_button_pressed() -> void:
+	upgrade_panel.states = upgrade_panel.states_enum.ENGINE
+
+
+func _on_n2o_button_pressed() -> void:
+	upgrade_panel.states = upgrade_panel.states_enum.N2O
+
+
+func _on_penetration_button_pressed() -> void:
+	upgrade_panel.states = upgrade_panel.states_enum.PENETRATION
+
+
+func _on_cancel_button_pressed() -> void:
+	upgrade_panel.states = upgrade_panel.states_enum.HIDE
+
+
+func _on_start_button_pressed() -> void:
+	PlayerManagement.save()
+	get_tree().change_scene_to_file("res://src/game/game.tscn")
