dfd0f7b8bbd529cd89e38a429bd2dd342c4c15c1
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-08-11 22:44:43 +0300

    added money label in basic ui

--- src/ui/basic_ui.gd
+++ src/ui/basic_ui.gd
@@ -1,6 +1,8 @@
 extends Control
 
 func _ready() -> void:
+	PlayerManagement.money_changed.connect(update_money_label)
+	update_money_label()
 	match WindowManagement.current_corner:
 		WindowManagement.BOTTOM_LEFT:
 			get_tree().get_first_node_in_group("pause_button").size_flags_horizontal = 0
@@ -16,3 +18,7 @@ func _on_texture_button_toggled(toggled_on: bool) -> void:
 		false:
 			$SettingsMargin.hide()
 			get_tree().paused = false
+
+
+func update_money_label():
+	$PauseMargin/VBoxContainer/Label.text = "$" + str(PlayerManagement.money)
