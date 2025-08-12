9cbd6b128462945c2174e0ee8d71709f16719218
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-08-12 22:36:56 +0300

    fixed ui not uiing

--- src/game/game.gd
+++ src/game/game.gd
@@ -1,7 +1,9 @@
 extends Node2D
 var end_position = Vector2.ZERO
 var fuel_active = true
+@onready var ui_preload = preload("res://src/ui/level_end_auto_ui.tscn")
 
 func _ready() -> void:
-	TransparentWindow.SetClickThrough(false)
+	if PlayerManagement.auto == true:
+		$basic_ui.add_child(ui_preload.instantiate())
 	Utils.game = self
