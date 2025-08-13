c627ddf421fcf797b6617cba4e10c2bb499a533a
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-08-13 22:45:59 +0300

    Create upgrade_prices_resource.gd

---
+++ src/resources/upgrade_prices_resource.gd
@@ -0,0 +1,6 @@
+class_name UpgradeResource
+extends Resource
+var fuel_price: Array[int] = [75, 225, 375, 500]
+var engine_price: Array[int] = [175, 325, 475, 600]
+var n2o_price: Array[int] = [150, 300, 450, 600]
+var penetration_price: Array[int] = [125, 250, 375, 500]
