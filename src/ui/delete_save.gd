850a1a773c645081f2c1b1697bf3223e5b709eeb
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-08-19 00:30:01 +0300

    delete save script added

---
+++ src/ui/delete_save.gd
@@ -0,0 +1,28 @@
+extends TextureButton
+var end_progress
+var is_impending_doom_initiated : bool = false
+
+
+func _on_button_down() -> void:
+	end_progress = Utils.game.get_node("basic_ui/TextureProgressBar")
+	end_progress.show()
+	is_impending_doom_initiated = true
+	$Timer.start()
+
+
+func _process(delta: float) -> void:
+	if is_impending_doom_initiated:
+		end_progress.value = ($Timer.time_left / $Timer.wait_time) * 100
+		print(get_global_mouse_position())
+		end_progress.global_position = get_global_mouse_position() + Vector2(10, 20)
+
+
+func _on_button_up() -> void:
+	$Timer.stop()
+	end_progress.hide()
+	is_impending_doom_initiated = false
+
+
+func _on_timer_timeout() -> void:
+	print("bb_save_:crying_emoji:")
+	PlayerManagement.delete_save()
