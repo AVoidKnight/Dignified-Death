e84dbee62c06b026d8836aae4215ac47fed6db3d
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-08-19 00:29:17 +0300

    auto end button added

--- src/ui/basic_ui.gd
+++ src/ui/basic_ui.gd
@@ -1,12 +1,16 @@
 extends Control
 
 func _ready() -> void:
+	if PlayerManagement.auto == true:
 		$BoostMargin/BoostProgressbar.hide()
+		if $"../..".name == "game":
+			$PauseMargin/TextureButton.show()
 	PlayerManagement.money_changed.connect(update_money_label)
 	update_money_label()
 	match WindowManagement.current_corner:
 		WindowManagement.BOTTOM_LEFT:
 			get_tree().get_first_node_in_group("pause_button").size_flags_horizontal = 0
+			get_tree().get_first_node_in_group("auto_end_button").size_flags_horizontal = 8
 		WindowManagement.BOTTOM_RIGHT:
 			get_tree().get_first_node_in_group("pause_button").size_flags_horizontal = 8
 
@@ -23,3 +27,8 @@ func _on_texture_button_toggled(toggled_on: bool) -> void:
 
 func update_money_label():
 	$PauseMargin/VBoxContainer/PanelContainer/Label.text = "$" + str(PlayerManagement.money)
+
+
+func _on_auto_end_button_pressed() -> void:
+	PlayerManagement.auto = false
+	get_tree().get_first_node_in_group("player").get_node("FuelComponent")._on_timer_timeout()
