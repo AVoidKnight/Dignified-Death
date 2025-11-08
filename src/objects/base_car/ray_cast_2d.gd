ddb7c30d4dfcc0a43bf1b2222c5f1116ca05c9e8
AVoidKnight <ilyuxa.andr0@gmail.com>
2025-11-09 00:08:14 +0300

    added car respawning after flipping

--- src/objects/base_car/ray_cast_2d.gd
+++ src/objects/base_car/ray_cast_2d.gd
@@ -1,8 +1,10 @@
 extends RayCast2D
 @onready var base_car = get_tree().get_first_node_in_group("player")
 var is_upside_down : bool = false
+var rerun_counter : int = 0
 
 func _ready() -> void:
+	rerun_counter = PlayerManagement.n2o
 	$Timer.start()
 
 
@@ -17,4 +19,11 @@ func _physics_process(_delta: float) -> void:
 		$Timer.paused = true
 
 func _on_timer_timeout() -> void:
+	if rerun_counter > 0:
+		get_parent().position.y -= 50;
+		get_parent().rotation = 0;
+		rerun_counter -= 1;
+		Utils.game.get_node("basic_ui/basic_ui").update_n2o_auto_icon(rerun_counter)
+		return
+	Utils.game.get_node("basic_ui/basic_ui").update_n2o_auto_icon(rerun_counter)
 	$"../FuelComponent"._on_timer_timeout()
